8bac7209e02e259937d958879fc0071dc1b2c2b7
Guillermo Martos <guille.l.martos@gmail.com>
2024-07-09 04:28:44 +0200

    - + fix port added to deploy solution use

--- src/config.ts
+++ src/config.ts
@@ -7,4 +7,4 @@ const __dirname = path.dirname(__filename);
 export const STATIC_PATH = path.join(__dirname, '..', 'public');
 export const HTML_FILES_PATH = path.join(STATIC_PATH, 'html');
 
-export const PORT = 3001;
+export const PORT = process.env.PORT || 3001;
